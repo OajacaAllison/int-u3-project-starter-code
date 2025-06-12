055fe2c2a117b60111b30b766d06bdb6f3ceea66
OajacaAllison <oajacaallison@gmail.com>
2025-06-12 14:13:20 -0400

    4etgeg

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let meow = document.querySelector(".fsubmit");
-let fakeform = document.querySelector(".urname");
-let 
+let jake = document.getElementById("real"); 
+document.addEventListener("DOMContentLoaded", function() {
+    let btn1 = document.getElementById("fakeBtn");
+    let f1 = document.querySelector(".hide");
+    let form2 = document.getElementById("urname");
 
-fsubmit.addEventListener("click", function() {
-    urname.style.display = "none";
+    btn1.addEventListener("click", function(){
+        f1.style.visibility = "hidden";
+        jake.style.visibility = "default";
+    })
 })
